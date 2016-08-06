fb43979ad2aaeeab54cc9e7ab6b01c3d61e8369b
Gipotalamus <hectop23@ukr.net>
2016-08-07 00:01:30 +0300

    added images validation

--- TalismanApplication/src/main/java/com/example/talisman/validators/PhotoValidator.java
+++ TalismanApplication/src/main/java/com/example/talisman/validators/PhotoValidator.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by gipotalamus on 15.07.16.
  */
@@ -19,8 +22,13 @@ public class PhotoValidator implements Validator {
     public void validate(Object o, Errors errors) {
         TalismanPhotoEntity photoEntity = (TalismanPhotoEntity) o;
         if (photoEntity.getTitle().trim().equals("")) {
-            System.out.println("error");
             errors.rejectValue("title", "validation.photo.title");
         }
+        String ext = photoEntity.getPhoto().split("\\.").length < 2?"" : photoEntity.getPhoto().split("\\.")[1];
+        Pattern pattern = Pattern.compile("(jpg)|(png)|(jpeg)|(bmp)");
+        Matcher matcher = pattern.matcher(ext);
+        if (photoEntity.getPhoto().trim().equals("")) errors.rejectValue("photo", "validation.member.photo.empty");
+        if (!matcher.matches()) errors.rejectValue("photo", "validation.member.photo.format");
+        if (photoEntity.getEvent().getTitle().trim().equals("")) errors.rejectValue("event", "validation.photo.event");
     }
 }
